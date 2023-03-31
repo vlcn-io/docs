dfb1d8e21a8ac926fa82014b37f76eb613c2586d
Matt <1009003+tantaman@users.noreply.github.com>
2023-03-31 12:36:11 -0400

    fully runnable and reacteive code!

---
+++ components/CodeNode.ts
@@ -0,0 +1,265 @@
+/**
+ * RunnableCode has imports and exports.
+ *
+ * Imports are what we need from other blocks.
+ * Exports are what we provide to blocks.
+ *
+ * Blocks with no imports can be run immediately.
+ * Blocks with imports must way till those imports are ready.
+ *
+ * We can create a cache of promises for each import.
+ *
+ * When someone exports, they can resolve that promise.
+ * When someone imports, they await on that promise.
+ *
+ * But it should likely be a generator and in a loop so we can re-run blocks and trigger
+ * a cascading update across all blocks.
+ */
+
+// TODO: rm from this map any resources no longer provided by anyone
+const resources = new Map<string, Resource>();
+
+const USE_REG = /^use\s+([A-z]{1}[A-z|0-9]*);$/gm;
+const PROVIDE_REG = /^provide\s+([A-z]{1}[A-z|0-9]*);$/gm;
+
+function getResouce(id: string) {
+  if (!resources.has(id)) {
+    resources.set(id, new Resource(id));
+  }
+  return resources.get(id);
+}
+
+export class CodeNode {
+  uses = new Map<string, number>();
+  provides = new Set<string>();
+  body: string | null = null;
+  pendingPromise: Promise<any> | null = null;
+  on: ((r: any, e?: any) => void) | null = null;
+
+  constructor() {}
+
+  eval(code: string) {
+    const [uses, body, provides] = this.parse(code);
+    this.body = body;
+    const noLongerUses = [...this.uses.keys()].filter((u) => !uses.has(u));
+
+    for (const u of noLongerUses) {
+      resources.get(u)?.off(this.onResourceReady);
+    }
+
+    this.uses = new Map();
+    for (const u of uses) {
+      this.uses.set(u, 0);
+    }
+    this.provides = provides;
+
+    for (const p of this.provides) {
+      getResouce(p)?.invalidate();
+    }
+
+    if (this.uses.size == 0) {
+      this.run(body);
+    }
+
+    for (const u of this.uses.keys()) {
+      getResouce(u)?.on(this.onResourceReady);
+    }
+  }
+
+  private run(body: string) {
+    let rejected = false;
+    let inputs = [];
+    for (const u of this.uses.keys()) {
+      const resource = getResouce(u);
+      this.uses.set(u, resource?.version || 0);
+      rejected = rejected || resource?.rejection !== undefined;
+      if (rejected) {
+        console.error(resource?.rejection);
+        return;
+      }
+      inputs.push(resource?.resolution);
+    }
+
+    const code = `async (${[...this.uses.keys()].join(", ")}) => {
+      let ___ret = await (async () => {
+        ${body}
+      })();
+
+      return [___ret, ${[...this.provides].join(", ")}];
+    }`;
+
+    const fn = eval(code);
+    const result = fn(...inputs);
+
+    let i = 1;
+    for (const p of this.provides) {
+      const resource = getResouce(p);
+      const index = i;
+      resource?.setPromise(
+        result.then((r: any) => {
+          return r[index];
+        })
+      );
+      ++i;
+    }
+
+    this.pendingPromise = result;
+    result.then(
+      (r: any) => {
+        if (this.pendingPromise != result) {
+          return;
+        }
+
+        if (isPromise(r[0])) {
+          r[0].then((r: any) => {
+            this.runComplete(r);
+          });
+        } else {
+          this.runComplete(r[0]);
+        }
+      },
+      (e: any) => {
+        this.runComplete(undefined, e);
+      }
+    );
+  }
+
+  private runComplete(result: any, e?: any) {
+    this.on?.(result, e);
+  }
+
+  private onResourceReady = (
+    id: string,
+    resolution: any,
+    rejection: any,
+    version: number
+  ) => {
+    // if all resources are ready, run
+    // but do not run once for each resoucrce binding
+    let allReady = true;
+    let seenAllVersions = true;
+
+    for (const [u, v] of this.uses) {
+      const resource = getResouce(u);
+      if (!resource?.isReady) {
+        allReady = false;
+        break;
+      }
+      if (v < resource.version) {
+        seenAllVersions = false;
+      }
+    }
+
+    if (!allReady) {
+      return;
+    }
+
+    if (!seenAllVersions) {
+      this.run(this.body!);
+    }
+  };
+
+  private parse(code: string): [Set<string>, string, Set<string>] {
+    const uses = new Set<string>();
+    for (const m of code.matchAll(USE_REG)) {
+      uses.add(m[1]);
+    }
+
+    const provides = new Set<string>();
+    for (const m of code.matchAll(PROVIDE_REG)) {
+      provides.add(m[1]);
+      code = code.replace(m[0], `self.${m[1]} = ${m[1]};`);
+    }
+
+    const body = code.replaceAll(USE_REG, "");
+
+    return [uses, body, provides];
+  }
+}
+
+type Consumer = (
+  id: string,
+  resolution: any,
+  rejection: any,
+  version: number
+) => void;
+
+class Resource {
+  private promise: Promise<any>;
+  public resolution: any;
+  public rejection: any;
+  private ready = false;
+  private consumers: Set<Consumer> = new Set();
+  private v = 1;
+
+  constructor(public readonly id: string) {
+    this.promise = Promise.resolve();
+  }
+
+  invalidate() {
+    this.promise = Promise.resolve();
+    this.resolution = undefined;
+    this.rejection = undefined;
+    this.ready = false;
+  }
+
+  setPromise(promise: Promise<any>) {
+    this.promise = promise;
+    this.resolution = undefined;
+    this.rejection = undefined;
+    this.ready = false;
+
+    promise.then(
+      (value) => {
+        if (promise == this.promise) {
+          ++this.v;
+          this.resolution = value;
+          this.ready = true;
+          this.notifyConsumers();
+        }
+      },
+      (error) => {
+        if (promise == this.promise) {
+          ++this.v;
+          this.rejection = error;
+          this.ready = true;
+          this.notifyConsumers();
+        }
+      }
+    );
+  }
+
+  on(consumer: Consumer) {
+    this.consumers.add(consumer);
+    if (this.isReady) {
+      consumer(this.id, this.resolution, this.rejection, this.version);
+    }
+    return () => this.off(consumer);
+  }
+
+  off(consumer: Consumer) {
+    this.consumers.delete(consumer);
+  }
+
+  private notifyConsumers() {
+    for (const c of this.consumers) {
+      c(this.id, this.resolution, this.rejection, this.version);
+    }
+  }
+
+  get isReady() {
+    return this.ready;
+  }
+
+  get version() {
+    return this.v;
+  }
+}
+
+function isPromise(obj: any) {
+  return (
+    !!obj &&
+    (typeof obj === "object" || typeof obj === "function") &&
+    typeof obj.then === "function"
+  );
+}
--- components/RunnableCode.tsx
+++ components/RunnableCode.tsx
@@ -1,213 +1,58 @@
-/**
- * RunnableCode has imports and exports.
- *
- * Imports are what we need from other blocks.
- * Exports are what we provide to blocks.
- *
- * Blocks with no imports can be run immediately.
- * Blocks with imports must way till those imports are ready.
- *
- * We can create a cache of promises for each import.
- *
- * When someone exports, they can resolve that promise.
- * When someone imports, they await on that promise.
- *
- * But it should likely be a generator and in a loop so we can re-run blocks and trigger
- * a cascading update across all blocks.
- */
+import { useEffect, useRef, useState } from "react";
+import createEditor from "./createEditor";
+import { EditorView } from "codemirror";
+import { CodeNode } from "./CodeNode";
 
-// TODO: rm from this map any resources no longer provided by anyone
-const resources = new Map<string, Resource>();
+export default function RunnableCode({ code }: { code: string }) {
+  const editorEl = useRef<HTMLDivElement>(null);
+  const editor = useRef<EditorView>();
+  const codeNode = useRef<CodeNode | null>(null);
+  const [result, setResult] = useState<any>();
+  const [error, setError] = useState<any | undefined>(undefined);
 
-export default function RunnableCode({ code }: { code: string }) {}
-
-const USE_REG = /^use\s+([A-z]{1}[A-z|0-9]*)$/gm;
-const PROVIDE_REG = /^provide\s+([A-z]{1}[A-z|0-9]*)$/gm;
-
-function getResouce(id: string) {
-  if (!resources.has(id)) {
-    resources.set(id, new Resource(id));
-  }
-  return resources.get(id);
-}
-
-class CodeNode {
-  uses = new Map<string, number>();
-  provides = new Set<string>();
-  body: string | null = null;
-
-  constructor(id: string, private code: string) {}
-
-  eval() {
-    const [uses, body, provides] = this.parse(this.code);
-    this.body = body;
-    const noLongerUses = [...this.uses.keys()].filter((u) => !uses.has(u));
-
-    for (const u of noLongerUses) {
-      resources.get(u)?.off(this.onResourceReady);
-    }
-
-    this.uses = new Map();
-    for (const u of uses) {
-      this.uses.set(u, 0);
-    }
-    this.provides = provides;
-
-    for (const p of this.provides) {
-      getResouce(p)?.invalidate();
-    }
-
-    if (this.uses.size == 0) {
-      this.run(body);
-    }
-
-    for (const u of this.uses.keys()) {
-      getResouce(u)?.on(this.onResourceReady);
-    }
-  }
-
-  private run(body: string) {
-    for (const u of this.uses.keys()) {
-      const resource = resources.get(u);
-      this.uses.set(u, resource?.version || 0);
-    }
-
-    const fn = `async (${[...this.uses].join(", ")}) => {
-      let ___ret = await (async () => {
-        ${body}
-      })();
-
-      return [___ret, ${[...this.provides].join(", ")}];
-    }`;
-
-    const result = eval(fn);
-  }
-
-  private onResourceReady = (
-    id: string,
-    resolution: any,
-    rejection: any,
-    version: number
-  ) => {
-    // if all resources are ready, run
-    // but do not run once for each resoucrce binding
-    let allReady = true;
-    let seenAllVersions = true;
-
-    for (const [u, v] of this.uses) {
-      const resource = resources.get(u);
-      if (!resource?.isReady) {
-        allReady = false;
-        break;
+  if (codeNode.current == null) {
+    codeNode.current = new CodeNode();
+    codeNode.current.on = (r: any, e?: any) => {
+      if (e) {
+        console.error(e);
+        setError(e);
+        setResult(undefined);
+        return;
       }
-      if (v < resource.version) {
-        seenAllVersions = false;
-      }
-    }
+      setError(undefined);
+      setResult(r);
+    };
+  }
 
-    if (!allReady) {
-      return;
-    }
+  const runCode = () => {
+    if (editor.current == null) return true;
 
-    if (seenAllVersions) {
-      return;
-    }
+    const code = editor.current.state.doc.toString();
+    codeNode.current?.eval(code);
 
-    this.run(this.body!);
+    return true;
   };
 
-  private parse(code: string): [Set<string>, string, Set<string>] {
-    const uses = new Set<string>();
-    for (const m of code.matchAll(USE_REG)) {
-      uses.add(m[1]);
-    }
+  useEffect(() => {
+    if (editorEl.current == null) return;
+    if (editor.current != null) return;
 
-    const provides = new Set<string>();
-    for (const m of code.matchAll(PROVIDE_REG)) {
-      provides.add(m[1]);
-    }
+    editor.current = createEditor({
+      parent: editorEl.current,
+      doc: code,
+      extraBinds: [{ key: "Shift-Enter", run: runCode }],
+    });
+    runCode();
+  }, [editorEl.current]);
 
-    const body = code.replaceAll(USE_REG, "").replaceAll(PROVIDE_REG, "");
-
-    return [uses, body, provides];
-  }
-}
-
-type Consumer = (
-  id: string,
-  resolution: any,
-  rejection: any,
-  version: number
-) => void;
-
-class Resource {
-  private promise: Promise<any>;
-  private resolution: any;
-  private rejection: any;
-  private ready = false;
-  private consumers: Set<Consumer> = new Set();
-  private v = 1;
-
-  constructor(public readonly id: string) {
-    this.promise = Promise.resolve();
-  }
-
-  invalidate() {
-    this.promise = Promise.resolve();
-    this.resolution = undefined;
-    this.rejection = undefined;
-    this.ready = false;
-  }
-
-  setPromise(promise: Promise<any>) {
-    this.promise = promise;
-    this.resolution = undefined;
-    this.rejection = undefined;
-    this.ready = false;
-
-    promise.then(
-      (value) => {
-        if (promise == this.promise) {
-          ++this.v;
-          this.resolution = value;
-          this.ready = true;
-          this.notifyConsumers();
-        }
-      },
-      (error) => {
-        if (promise == this.promise) {
-          ++this.v;
-          this.rejection = error;
-          this.ready = true;
-          this.notifyConsumers();
-        }
-      }
-    );
-  }
-
-  on(consumer: Consumer) {
-    if (this.isReady) {
-      consumer(this.id, this.resolution, this.rejection, this.version);
-    }
-    this.consumers.add(consumer);
-    return () => this.off(consumer);
-  }
-
-  off(consumer: Consumer) {
-    this.consumers.delete(consumer);
-  }
-
-  private notifyConsumers() {
-    for (const c of this.consumers) {
-      c(this.id, this.resolution, this.rejection, this.version);
-    }
-  }
-
-  get isReady() {
-    return this.ready;
-  }
-
-  get version() {
-    return this.v;
-  }
+  return (
+    <div>
+      <div ref={editorEl}></div>
+      <pre style={{ display: result !== undefined ? "block" : "none" }}>
+        <code>
+          {error !== undefined ? error?.message : JSON.stringify(result)}
+        </code>
+      </pre>
+    </div>
+  );
 }
