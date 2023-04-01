4dd27006a167b6fe2f35fda6fced381695596a1a
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-01 14:06:48 -0400

    more docs! js api docs!

--- theme.config.jsx
+++ theme.config.jsx
@@ -9,5 +9,15 @@ export default {
     forcedTheme: "light",
   },
   docsRepositoryBase: "https://github.com/tantaman/vlcn-nextra/blob/main",
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://vlcn.io" target="_blank">
+          One Law LLC
+        </a>
+      </span>
+    ),
+  },
   // ...
 };
