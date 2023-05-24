b10fb03af7aeb51494bd0b877785c05d32b117fa
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-24 12:58:57 -0400

    jq to process package.json

--- update-pkgs.sh
+++ update-pkgs.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 declare -a pkgs=(
   "@vlcn.io/crsqlite-wasm:0.10.2-next.1"
   "@vlcn.io/react:0.10.3-next.4"
@@ -9,10 +11,12 @@ declare -a pkgs=(
 for pkg in "${pkgs[@]}"; do
   echo "Updating $pkg"
   
-  IFS=':' read -ra parts <<< "$pkg"
+  IFS=':' read -r NAME VERSION <<< "$pkg"
   
   # replace in package.json
-  sed -i '' -e "s/\"${parts[0]}\": \".*\"/\"${parts[0]}\": \"${parts[1]}\"/g" ./package.json
+  # echo "s/\"${parts[0]}\": \".*\"/\"${parts[0]}\": \"${parts[1]}\"/g"
+  jq --arg name "$NAME" --arg version "$VERSION" '.dependencies[$name] = $version' package.json > tmp.json && mv tmp.json package.json
   # replace in all mdx files in pages and contained directories
-  find ./pages -name "*.mdx" -exec sed -i '' -e "s/${parts[0]}@.*\"/${parts[0]}@${parts[1]}\"/g" {} \;
+  # find ./pages -name "*.mdx" -exec sed -i '' -e "s/${parts[0]}\@.*\"/${parts[0]}\@${parts[1]}\"/g" {} \;
+  find ./pages -name "*.mdx" -exec sed -i -E "s/${NAME}@[^[:space:]]+/${NAME}@${VERSION}/g" {} \;
 done
