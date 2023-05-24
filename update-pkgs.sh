2dd8bfa417f57833da7165f2801d2cb99d1a3646
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-24 13:45:59 -0400

    sub-fn

--- update-pkgs.sh
+++ update-pkgs.sh
@@ -8,6 +8,13 @@ declare -a pkgs=(
   "@vlcn.io/wa-sqlite:0.16.1"
 )
 
+update_file() {
+  local file=$1
+  local name=$2
+  local version=$3
+  awk -v name="$name" -v version="$version" '{gsub(name "@[^[:space:]]+", name "@" version); print}' "$file" > tmp && mv tmp "$file"
+}
+
 for pkg in "${pkgs[@]}"; do
   echo "Updating $pkg"
   
@@ -18,7 +25,15 @@ for pkg in "${pkgs[@]}"; do
   jq --arg name "$NAME" --arg version "$VERSION" '.dependencies[$name] = $version' package.json > tmp.json && mv tmp.json package.json
   # replace in all mdx files in pages and contained directories
   # find ./pages -name "*.mdx" -exec sed -i '' -e "s/${parts[0]}\@.*\"/${parts[0]}\@${parts[1]}\"/g" {} \;
-  # find ./pages -name "*.mdx" -exec sed -i -E "s/${NAME}@[^[:space:]]+/${NAME}@${VERSION}/g" {} \;
-  find "$DIRECTORY" -name '*.mdx' -exec awk -v name="$NAME" -v version="$VERSION" '{gsub(name "@[^[:space:]]+", name "@" version); print}' {} \; > tmp && mv tmp {}
+  # find ./pages -name "*.mdx" -exec sed -i -E "s/${NAME}@[^0-9a-z.]+/${NAME}@${VERSION}/g" {} \;
+  find ./pages -name '*.mdx' -exec bash -c '
+      update_file() {
+        local file=$1
+        local name=$2
+        local version=$3
+        awk -v name="$name" -v version="$version" "{gsub(name \"@[0-9a-z.\-]+\", name \"@\" version); print}" "$file" > tmp && mv tmp "$file"
+      }
+      update_file "$0" "$1" "$2"
+    ' {} "$NAME" "$VERSION" \;
 
 done
