8635c144c644c274ee80f6abe0b475e4479a96c6
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-03 09:43:52 -0400

    base html and body classes, logo img sizes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,39 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-8C2G4330W2"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-8C2G4330W2');
-        `}
-        </Script>
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage;
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
+    return (
+      <Html className={pageProps.htmlClass} lang="en">
+        <Head />
+        <body className={pageProps.bodyClass}>
+          <Main />
+          <NextScript />
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-8C2G4330W2"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){window.dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-8C2G4330W2');
+         `}
+          </Script>
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,35 @@
+import Image from "next/image";
+import style from "./index.module.css";
+import { useEffect } from "react";
+
+export async function getStaticProps() {
+  return { props: { bodyClass: style.body, htmlClass: style.html } };
+}
+
 export default function Index() {
-  return <div>Ello</div>;
+  useEffect(() => {
+    document.querySelector("html")?.classList.add(style.html);
+    document.querySelector("body")?.classList.add(style.body);
+    return () => {
+      document.querySelector("html")?.classList.remove(style.html);
+      document.querySelector("body")?.classList.remove(style.body);
+    };
+  }, []);
+  return (
+    <div className={style.root}>
+      <header className={style.header}>
+        <div className={style.logoImg}>
+          <Image
+            alt="Vulcan forging new weapons, in the battle against compleixty, on an anvil"
+            src="/logo.png"
+            fill
+            priority
+            style={{ objectFit: "contain" }}
+            sizes="(max-width: 1600px) 12vw, (max-width: 800px) 20vw, 6vw"
+          />
+        </div>
+      </header>
+      <section className={style.hero}></section>
+    </div>
+  );
 }
